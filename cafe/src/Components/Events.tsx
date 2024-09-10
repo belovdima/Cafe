ef701479f0cd67d8727227f7389cd62468c82468
Dmitry Belov <machete75111@gmail.com>
2024-09-10 11:00:10 +0300

    started events component

--- cafe/src/Components/Events.tsx
+++ cafe/src/Components/Events.tsx
@@ -3,9 +3,25 @@ import "./../Styles/SCSS/_Events.scss"
 
 function Events() {
 
+
+
     return (
-        <div>
-            events block
+        <div className="events">
+            <div className="carousel">
+                <div className="carousel__inner">
+                    <div className="carousel__inner--item">
+                        <img src="~/../../pics/fish.jpg" alt="fish" />
+                    </div>
+                    <div className="carousel__inner--item">
+                        <img src="~/../../pics/tyler.jpg" alt="tyler" />
+                    </div>
+                    <div className="carousel__inner--item">
+                        <img src="~/../../pics/minecraft.jpg" alt="minecraft" />
+                    </div>
+                    
+                </div>
+                
+            </div>
         </div>
     );
 }
